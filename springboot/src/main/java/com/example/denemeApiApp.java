258b299e7cb7c2f1dc54e640889ee51289395540
Ali Katırcıo <ali.katircio@gmail.com>
2017-11-20 23:56:03 +0300

    createapplicationpage

--- springboot/src/main/java/com/example/denemeApiApp.java
+++ springboot/src/main/java/com/example/denemeApiApp.java
@@ -1,7 +1,17 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by Ali on 19.11.2017.
  */
+
+@SpringBootApplication
 public class denemeApiApp {
+
+    public static void main(String[] args){
+
+        SpringApplication.run(denemeApiApp.class, args);//spring boot uygulaması için kullanolır.
+    }
 }
