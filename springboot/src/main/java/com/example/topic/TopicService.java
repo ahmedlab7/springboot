289f1a7c5e6d5e1a3090cef03b96be41bdac9f8d
Ali Katırcio <ali.katircio@pia-team.com>
2017-12-12 15:03:34 +0300

    post get put delete update

--- springboot/src/main/java/com/example/topic/TopicService.java
+++ springboot/src/main/java/com/example/topic/TopicService.java
@@ -2,6 +2,7 @@ package com.example.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,11 +13,11 @@ import java.util.List;
 @Service
 public class TopicService {
 
-private List<Topic> topics = Arrays.asList(
+private List<Topic> topics = new ArrayList<Topic>(Arrays.asList(
         new Topic("spring", "spring framework", "Spring framework description"),
         new Topic("java", "cora java", "core java description"),
         new Topic("javascript", "Javascript", "javascript  description")
-        );
+        ));
 
     public List<Topic> getAllTopics(){
 
@@ -33,4 +34,32 @@ private List<Topic> topics = Arrays.asList(
         return null;
     }
 
+    public void addTopic(Topic topic) {
+
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+
+        for (int i = 0; i<topics.size(); i++){
+
+            Topic t = topics.get(i);
+            if(t.getId().equals(id)){
+
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+
+        for (int i = 0; i<topics.size(); i++){
+
+            if (topics.get(i).getId().equals(id)){
+
+                topics.remove(i);
+            }
+        }
+    }
 }
